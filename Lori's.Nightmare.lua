b9e766c499992f0a4093dc98330e83159ddd2caf
Group of Marmeladov Studios <d3660379@gmail.com>
2025-12-18 19:03:25 +0300

    Update payload URL for script retrieval

--- Lori's.Nightmare.lua
+++ Lori's.Nightmare.lua
@@ -1,4 +1,4 @@
-local payload_url = "ВАША_RAW_ССЫЛКА_С_GITHUB_НАЧИНАЮЩАЯСЯ_С_https://raw.githubusercontent.com/..." 
+local payload_url = "https://raw.githubusercontent.com/d3660379-rgb/Turbo-hubs-scripts/refs/heads/main/Turbo-BY-Lori-s-Nightmare-script" 
 
 local success, obfuscated_code = pcall(function()
     return game:HttpGet(payload_url, true)
